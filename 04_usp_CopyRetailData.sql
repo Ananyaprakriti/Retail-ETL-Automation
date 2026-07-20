4d54368578a788ed66b1e77132b715f8e5e90cfb
Ananyaprakriti <Ananyaasukameti2301@gmail.com>
2026-07-21 00:16:05 +0530

    Enhance usp_CopyRetailData with ETL audit logging
    Added audit logging for ETL process success and failure.

--- 04_usp_CopyRetailData.sql
+++ 04_usp_CopyRetailData.sql
@@ -13,74 +13,89 @@ BEGIN
 
         BEGIN TRANSACTION;
 
-       
-       TRUNCATE TABLE Customers;
-
-        INSERT INTO Customers
-        SELECT *
-        FROM Retail_SourceDB.dbo.Customers;
-
-        SET @RecordsProcessed = @RecordsProcessed + @@ROWCOUNT;
-
-
         
-        TRUNCATE TABLE Products;
-
-        INSERT INTO Products
-        SELECT *
-        FROM Retail_SourceDB.dbo.Products;
-
+        TRUNCATE TABLE Customers;
+        INSERT INTO Customers
+        SELECT * FROM Retail_SourceDB.dbo.Customers;
         SET @RecordsProcessed = @RecordsProcessed + @@ROWCOUNT;
 
+      
+        TRUNCATE TABLE Products;
+        INSERT INTO Products
+        SELECT * FROM Retail_SourceDB.dbo.Products;
+        SET @RecordsProcessed = @RecordsProcessed + @@ROWCOUNT;
 
        
         TRUNCATE TABLE Employees;
-
         INSERT INTO Employees
-        SELECT *
-        FROM Retail_SourceDB.dbo.Employees;
-
+        SELECT * FROM Retail_SourceDB.dbo.Employees;
         SET @RecordsProcessed = @RecordsProcessed + @@ROWCOUNT;
-
-
-       
-        TRUNCATE TABLE Suppliers;
-
-        INSERT INTO Suppliers
-        SELECT *
-        FROM Retail_SourceDB.dbo.Suppliers;
-
-        SET @RecordsProcessed = @RecordsProcessed + @@ROWCOUNT;
-
 
       
+        TRUNCATE TABLE Suppliers;
+        INSERT INTO Suppliers
+        SELECT * FROM Retail_SourceDB.dbo.Suppliers;
+        SET @RecordsProcessed = @RecordsProcessed + @@ROWCOUNT;
+
+       
         TRUNCATE TABLE Inventory;
-
         INSERT INTO Inventory
-        SELECT *
-        FROM Retail_SourceDB.dbo.Inventory;
-
+        SELECT * FROM Retail_SourceDB.dbo.Inventory;
         SET @RecordsProcessed = @RecordsProcessed + @@ROWCOUNT;
 
-
-       
+        -- Orders
         TRUNCATE TABLE Orders;
-
         INSERT INTO Orders
-        SELECT *
-        FROM Retail_SourceDB.dbo.Orders;
-
+        SELECT * FROM Retail_SourceDB.dbo.Orders;
         SET @RecordsProcessed = @RecordsProcessed + @@ROWCOUNT;
 
-
-       
         COMMIT TRANSACTION;
+
+        -- ETL Audit Success
+        INSERT INTO ETL_Audit
+        (
+            ProcedureName,
+            StartTime,
+            EndTime,
+            Status,
+            RecordsProcessed,
+            ErrorMessage
+        )
+        VALUES
+        (
+            'usp_CopyRetailData',
+            @StartTime,
+            GETDATE(),
+            'SUCCESS',
+            @RecordsProcessed,
+            NULL
+        );
 
     END TRY
 
     BEGIN CATCH
 
         ROLLBACK TRANSACTION;
+
+        -- ETL Audit Failure
+        INSERT INTO ETL_Audit
+        (
+            ProcedureName,
+            StartTime,
+            EndTime,
+            Status,
+            RecordsProcessed,
+            ErrorMessage
+        )
+        VALUES
+        (
+            'usp_CopyRetailData',
+            @StartTime,
+            GETDATE(),
+            'FAILED',
+            0,
+            ERROR_MESSAGE()
+        );
 
         THROW;
 
